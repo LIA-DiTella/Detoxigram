b92bc185242fba99c417d3524e5545cb5d2b95d3
Luz Alba Posse <luzalbaposse@gmail.com>
2024-01-22 16:02:54 -0300

    update

--- script-dowload-channels/processjson.py
+++ script-dowload-channels/processjson.py
@@ -15,33 +15,30 @@ def process_json(input_file, output_file):
         Creates or overwrites the output_file with the transformed message data.
 
     Returns:
-        None: It writes the output directly to the specified output_file. The output JSON contains a dictionary with message IDs as keys, and each value is a dictionary with 'message' and
-              'timestamp' keys extracted and transformed from the input file.
+        None: It writes the output directly to the specified output_file. The output JSON contains a list of dictionaries with 'message' and 'timestamp' keys extracted and transformed from the input file.
     '''
     # Read the JSON file
     with open(input_file, 'r') as file:
         data = json.load(file)
 
-    # Extract messages
-    messages = data.get('messages', [])
+    # Extract messages and keep the last 100
+    messages = data.get('messages', [])[-100:]
 
     # Transform data
-    transformed_data = {}
+    transformed_data = []
     for message in messages:
         if message.get('_') == 'Message':
-            message_id = message.get('id')
             text = message.get('message')
             timestamp = message.get('date')
 
-            # Convert timestamp to a standard format (optional)
+            # Convert timestamp to a standard format
             try:
                 timestamp = datetime.fromisoformat(timestamp).strftime('%Y-%m-%d %H:%M:%S')
             except ValueError:
                 pass  # Keep the original format if conversion fails
 
-            transformed_data[message_id] = {'message': text, 'timestamp': timestamp}
+            transformed_data.append({'message': text, 'timestamp': timestamp})
 
     # Write transformed data to a new JSON file
     with open(output_file, 'w') as file:
         json.dump(transformed_data, file, indent=4)
-
