08dd703cc7c8d6210ddb4a2ad9aece0f867f5bef
Luz Alba Posse <luzalbaposse@gmail.com>
2024-02-29 13:21:49 -0300

    Update botv2.py
    Mejora a la ux :)

--- detoxigram-bot/botv2.py
+++ detoxigram-bot/botv2.py
@@ -1,11 +1,15 @@
 '''
--> Avisar q se toman los ultimos 50 mensajes ?
+-> Avisar q se toman los ultimos 50 mensajes ?  ✅
 -> Ir para atras y elegir random ? 
        - Ultimas horas o toda la historia del canal ?
 - limite cuando se deje de sentir q es en tiempo real.
 - cuando este cerrado chequear tiempos de respuesta
+-> WExplain me the toxicity level -> cambiar wording
+-> show me an example -> mediana del canal -> would you like to see a detoxify version -> show me another example -> desues de 3 nada más. 
+->  show more -> detoxify my messages. 
 '''
 import sys
+import time
 sys.path.append('..')
 import os
 from dotenv import load_dotenv
@@ -38,9 +42,9 @@ llm = ChatOpenAI(model="gpt-3.5-turbo", temperature = 0)
 output_parser = StrOutputParser()
 
 #Initialize classifiers
-perspective = perspective_classifier(GOOGLE_CLOUD_API_KEY ,attributes=["TOXICITY"], verbosity=True)
+# perspective = perspective_classifier(GOOGLE_CLOUD_API_KEY ,attributes=["TOXICITY"], verbosity=True)
 bert = hate_bert_classifier("../model_evaluation_scripts/classifiers_classes_api/toxigen_hatebert", verbosity = True)
-gpt = gpt_classifier("gpt-3.5-turbo", OPENAI_API_KEY, verbosity = True)
+# gpt = gpt_classifier("gpt-3.5-turbo", OPENAI_API_KEY, verbosity = True)
 #Check the token
 print("Token:", BOT_TOKEN)
 
@@ -52,6 +56,9 @@ loop = asyncio.new_event_loop()
 
 # Initialize the Telethon client with MemorySession
 client = TelegramClient(sessions.MemorySession(), API_ID_TELEGRAM, API_HASH_TELEGRAM)
+
+#Some global variables
+last_analyzed_toxicity = {}
 
 #Use the telegram API for fetching the last 50 messages of a channel (this will need verification)
 async def fetch_last_50_messages(channel_name) -> list:
@@ -89,12 +96,15 @@ def process_messages(messages) -> list:
             }
             processed_messages.append(processed_message)
     return processed_messages
-
 def summarizor_gpt(data, channelname) -> str:
     transformed_data = transform_data_to_expected_format(data)
     print(transformed_data)
     if channelname:
+            start = time.time()
             messages = loop.run_until_complete(fetch_last_50_messages(channelname))
+            end = time.time()
+            print(f"Time to fetch messages: {end - start}")
+            bot.reply_to(channelname, "I've got the messages! Now give me one second to summarize them... 🕣")
             processed_messages = process_messages(messages)
             if len(processed_messages) > 0:
                 data = processed_messages[:20]
@@ -211,16 +221,42 @@ def summarizor_gpt(data, channelname) -> str:
     output = chain.batch([{}])
     return output
 def helper(message):
-    bot.reply_to(message, "Helping!")
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    summarize = types.InlineKeyboardButton('Summarize a group chat 📝', callback_data='summarize')
+    explainer = types.InlineKeyboardButton('Explain me the toxicity levels 👀', callback_data='explain')
+    analyze = types.InlineKeyboardButton('Analyze a group chat 🔍', callback_data='analyze')
+    go_back = types.InlineKeyboardButton('Start again! 🔄', callback_data='restart')
+    help_text = (
+        "Here's how you can use Detoxigram:\n\n"
+        "1. **Analyze a Group Chat:** Tap the 'Analyze a group chat 🔍' button and provide the @ChannelName to analyze the toxicity of the channel's messages.\n\n"
+        "2. **Summarize a Group Chat:** Tap the 'Summarize a group chat 📝' button and provide the @ChannelName to get a summary of the channel's messages and an evaluation of its toxicity level.\n\n"
+        "3. **Understanding Toxicity Levels:** Tap the 'Explain me toxicity levels 👀' button to learn more about the different levels of toxicity.\n\n"
+        "If you have any questions or need further assistance, feel free to ask!"
+        "You can reach us out at malbaposse@mail.utdt.edu\n\n"
+    )
+    markup.add(analyze, summarize, explainer, go_back)
+    bot.reply_to(message, help_text)
+
 def analyze_channel_bert(message): 
     try:
         channel_name = message.text
         if channel_name:
+            bot.reply_to(message, f"Got it! We will analyze {channel_name}... Please wait a moment 🙏")
+            start = time.time()
             messages = loop.run_until_complete(fetch_last_50_messages(channel_name))
             processed_messages = process_messages(messages)
+            end = time.time()
+
+            if processed_messages:
+                if end - start > 10:
+                    bot.reply_to(message, "It took a while to get the messages, but I've got them! Now give me one second to predict the toxicity of the channel... 🕣")
+                else:
+                    bot.reply_to(message, "I've got the messages! Now give me some seconds to predict the toxicity of the channel... 🕣")
             markup = types.InlineKeyboardMarkup(row_width=1)
             summarize = types.InlineKeyboardButton('Summarize a group chat 📝', callback_data='summarize')
-            explainer = types.InlineKeyboardButton('Explain me toxicity levels 👀', callback_data='explain')
+            explainer = types.InlineKeyboardButton('Explain me the toxicity levels 👀', callback_data='explain')
+            go_back = types.InlineKeyboardButton('Start again! 🔄', callback_data='restart')
+            new_analyze = types.InlineKeyboardButton('Analyze another group chat 🔍', callback_data='analyze')
             if len(processed_messages) > 0:
                 data = processed_messages[:50]
                 total_toxicity_score=0
@@ -229,14 +265,15 @@ def analyze_channel_bert(message):
                     toxicity_score, numeric_toxicity = toxicity_result
                     total_toxicity_score += numeric_toxicity
                 average_toxicity_score = total_toxicity_score / len(messages)
+                last_analyzed_toxicity[message.chat.id] = average_toxicity_score
                 if 0 <= average_toxicity_score < 0.99:
-                        markup.add(explainer, summarize)
+                        markup.add(explainer, summarize, new_analyze, go_back)
                         bot.send_message(message.chat.id, f'''🟢 Well, {channel_name} doesn't seem to be toxic at all!''', reply_markup=markup)
                 elif 0.99 <= average_toxicity_score < 2.1:
-                        markup.add(explainer, summarize)
+                        markup.add(explainer, summarize, new_analyze, go_back)
                         bot.send_message(message.chat.id, f'''🟡 Watch out! {channel_name} seems to have a moderately toxic content''', reply_markup=markup)
                 elif 2.1 <= average_toxicity_score < 4.1:
-                        markup.add(explainer, summarize)
+                        markup.add(explainer, summarize, new_analyze, go_back)
                         bot.send_message(message.chat.id, f'''🔴 Be careful... {channel_name} seems to have a highly toxic content''', reply_markup=markup)
             else:
                 bot.reply_to(message, "No messages found in the specified channel! Why don't we try again with another channel? Type /start and we will start all over again :)", reply_markup=markup)
@@ -254,18 +291,18 @@ def summarize(message):
             messages = loop.run_until_complete(fetch_last_50_messages(channel_name))
             processed_messages = process_messages(messages)
             if len(processed_messages) > 0:
-                bot.reply_to(message, "Let's see... 🤔")
+                bot.reply_to(message, "Give me one sec... 🤔")
                 data = processed_messages[:20]
-                output = summarizor_gpt(data, message)
+                output = summarizor_gpt(data, channel_name)
                 print(output)
                 bot.reply_to(message, f'{output[0]}')
             else:
                 bot.reply_to(message, f'Failed! Try with another channel!')
         else:
             bot.reply_to(message, "Please provide a channel name!")
-    except IndexError:
-        bot.reply_to(message, "Usage: /summarize @channelname")
 
+    except:
+        bot.reply_to(message, "An error occurred! Please try again!")
 #start is the command used for saying hi to the user and giving him/her instructions
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -280,30 +317,45 @@ I\'m here to help you to identify toxicity in your telegram channels, so you can
 
 @bot.callback_query_handler(func=lambda call: True)
 def answer(callback):
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    summarize = types.InlineKeyboardButton('Summarize a group chat 📝', callback_data='summarize')
+    explainer = types.InlineKeyboardButton('Explain me the toxicity levels 👀', callback_data='explain')
+    analyze = types.InlineKeyboardButton('Analyze a group chat 🔍', callback_data='analyze')
+    go_back = types.InlineKeyboardButton('Start again! 🔄', callback_data='restart')
+    new_analyze = types.InlineKeyboardButton('Analyze another group chat 🔍', callback_data='analyze')
+            
     if callback.message:
         if callback.data == 'analyze':
-            bot.send_message(callback.message.chat.id, "Great! Please provide the @ChannelName you would like to analyze 🤓")
+            bot.send_message(callback.message.chat.id, "Great! Just for you to know, when we evaluate the toxicity, we'll only consider the last 50 messages of the channel ⚠️ Now, please provide the @ChannelName you would like to analyze 🤓")
             bot.register_next_step_handler(callback.message, analyze_channel_bert)
         elif callback.data == 'summarize':
-            bot.send_message(callback.message.chat.id, "Great! Please provide the @ChannelName you would like to summarize 🤓")
+            bot.send_message(callback.message.chat.id, "Let's do this! Please provide the @ChannelName you would like to summarize 🤓")
             bot.register_next_step_handler(callback.message, summarize)
         elif callback.data == 'help':
             helper(callback.message)
         elif callback.data == 'end':
             bot.send_message(callback.message.chat.id, "Goodbye! 👋 If you need anything else, just type /start")
+        elif callback.data == 'restart':
+
+            markup.add(analyze, summarize, explainer)
+            bot.send_message(callback.message.chat.id, "Ok! Let's see, what would you like to do now? 🤔")
         elif callback.data == 'explain':
-            bot.send_message(callback.message.chat.id, '''🚫 Understanding Toxicity Levels:\n
+            toxicity_level = last_analyzed_toxicity.get(callback.message.chat.id, None)
+            if toxicity_level is not None:
+                if 0 <= toxicity_level < 0.99:
+                    explanation = "🟢 Non-toxic: Messages that foster a positive, respectful, and inclusive atmosphere, promoting kindness and mutual understanding. They value diverse opinions and contribute to constructive dialogue without offensive content. ✨\n"
+                elif 0.99 <= toxicity_level < 1.5:
+                    explanation = "🟡 Slightly Toxic: Messages that, while mostly respectful, may include criticism or disagreements in a passive-aggressive manner. They lack full appreciation for differing viewpoints but don't directly attack individuals or groups. 🤔\n"
+                elif 1.5 <= toxicity_level < 2.5:
+                    explanation = "🟡 Moderately Toxic: Messages with an aggressive or disrespectful tone, often containing sarcasm, irony, or derogatory language towards specific groups. They seek to hurt, ridicule, or belittle others, showing a rejection of diversity. 😠\n\n"
+                elif 2.5 <= toxicity_level < 3.2:
+                    explanation = "🔴 Highly Toxic: Messages demonstrating clear rejection and contempt for individuals or groups, using insults, degrading language, or references based on gender, ethnicity, etc. They aim to intimidate, exclude, or incite hatred, with explicit intent to cause harm. 😡\n"
+                elif 3.2 <= toxicity_level < 4:
+                    explanation = "🔴 Extremely Toxic: Messages explicitly aggressive and disrespectful, containing threats or calls to violent action. They promote hostility, incite hatred, and suggest harmful consequences, endangering the safety and well-being of others. ⚠️ \n 😡\n"
 
-Non-toxic: Messages that foster a positive, respectful, and inclusive atmosphere, promoting kindness and mutual understanding. They value diverse opinions and contribute to constructive dialogue without offensive content. ✨\n
-Slightly Toxic: Messages that, while mostly respectful, may include criticism or disagreements in a passive-aggressive manner. They lack full appreciation for differing viewpoints but don't directly attack individuals or groups. 🤔\n
-
-Moderately Toxic: Messages with an aggressive or disrespectful tone, often containing sarcasm, irony, or derogatory language towards specific groups. They seek to hurt, ridicule, or belittle others, showing a rejection of diversity. 😠\n
-
-Highly Toxic: Messages demonstrating clear rejection and contempt for individuals or groups, using insults, degrading language, or references based on gender, ethnicity, etc. They aim to intimidate, exclude, or incite hatred, with explicit intent to cause harm. 😡\n
-
-Extremely Toxic: Messages explicitly aggressive and disrespectful, containing threats or calls to violent action. They promote hostility, incite hatred, and suggest harmful consequences, endangering the safety and well-being of others. ⚠️ \n
-
-Understanding these levels helps maintain a healthy and respectful online environment. Let's strive for constructive dialogue and mutual respect. 🌐💬
-
-''')
+                message_text = explanation + "\nUnderstanding these levels helps maintain a healthy and respectful online environment. Let's strive for constructive dialogue and mutual respect. 🌐💬"
+                markup.add(new_analyze, summarize, go_back)
+                bot.send_message(callback.message.chat.id, message_text)
+            else:
+                bot.send_message(callback.message.chat.id, "No toxicity level has been analyzed yet for this chat! ")
 bot.infinity_polling()
