72cf1f06af12e9950769e8492b4a202d55ab4585
Luz Alba Posse <luzalbaposse@gmail.com>
2024-07-24 13:23:38 -0300

    Update Toxicity.py

--- refactor/toxicity/Toxicity.py
+++ refactor/toxicity/Toxicity.py
@@ -10,6 +10,7 @@ class Toxicity:
         self.analyzer = Analyzer(model_name)
         self.detoxifier = Detoxifier(model_name)
         self.explainer = Explainer(llm, detoxigramer)
+        self.dataviz = ToxicityDataviz()
     
     def analyze_toxicity(self, messages: list) -> list:
         return [self.analyzer.analyze(message) for message in messages]
@@ -19,3 +20,6 @@ class Toxicity:
     
     def explain_toxicity(self, messages: list, escala: str, toxicity: int) -> list:
         return self.explainer.explain_toxicity(messages, escala, toxicity)
+    
+    def dimensions_toxicity(self, conversation : str, conversation_name : str, toxicity : int):
+        return self.dataviz.get_toxicity_dimensions(conversation, conversation_name, toxicity)
