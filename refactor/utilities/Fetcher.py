c744707135c8397cd669c98beead0cb7d58ffd63
PattoPeraltta <136608427+PattoPeraltta@users.noreply.github.com>
2024-08-01 20:57:44 -0300

    Finished messager

--- refactor/utilities/Fetcher.py
+++ refactor/utilities/Fetcher.py
@@ -94,13 +94,3 @@ class WhatsApp_Fetcher:
         else:
             raise FileNotFoundError("No .txt file found in the ZIP archive")
 
-
-
-API_HASH_TELEGRAM:str = "b6f8fbecc6568cf05834f9419582e8ca"
-API_ID_TELEGRAM:str = "27486167"
-client:TelegramClient = TelegramClient(sessions.MemorySession(), API_ID_TELEGRAM, API_HASH_TELEGRAM)  
-
-telegram = Telegram_Fetcher(client)
-messages = asyncio.run(telegram.fetch('@TrumpJr'))
-print(messages)
-
