0a6ed7f8eeb97a7efc59f349b39f44c733786aa6
Luz Alba Posse <luzalbaposse@gmail.com>
2024-08-13 11:02:43 -0300

    Delete Refactor Folder
    Just move the folders outside refactor.
    
    Co-Authored-By: Pato Peralta <136608427+PattoPeraltta@users.noreply.github.com>

--- telegram/_telegram.py
+++ telegram/_telegram.py
@@ -21,6 +21,7 @@ from toxicity.Explainer import Explainer
 from toxicity.Analyzer import Analyzer
 from toxicity.Detoxifier import Detoxifier
 from toxicity.Dataviz import ToxicityDataviz
+from user_management.ManagementDetoxigramers import ManagementDetoxigramers
 from user_management.Detoxigramer import Telegram_Detoxigramer
 
 # Variables de Ambiente
@@ -28,22 +29,22 @@ from user_management.Detoxigramer import Telegram_Detoxigramer
 BOT_TOKEN:str = os.environ.get('BOT_TOKEN')
 API_ID_TELEGRAM:str = os.environ.get('API_ID')
 API_HASH_TELEGRAM:str = os.environ.get('API_HASH')
-MISTRAL_API_KEY:str = os.environ['MISTRAL_API_KEY']
+MISTRAL_API_KEY:str = os.environ('MISTRAL_API_KEY')
 
 # Modelos
 
 hatebert:hate_bert_classifier = hate_bert_classifier('../model_evaluation_scripts/classifiers_classes_api/toxigen_hatebert', verbosity=True)
 multibert:multi_bert_classifier = multi_bert_classifier('../model_evaluation_scripts/classifiers_classes_api/multibert', verbosity=True, toxicity_distribution_path='../model_evaluation_scripts/classifiers_classes_api/toxicity_distribution_cache/multibert_distribution.json',calculate_toxicity_distribution=False)
 mistral:mistral_classifier = mistral_classifier(mistral_api_key=MISTRAL_API_KEY, templatetype='prompt_template_few_shot', verbosity=True, toxicity_distribution_path='../model_evaluation_scripts/classifiers_classes_api/toxicity_distribution_cache/mistral_distribution.json', calculate_toxicity_distribution=False)
-
+output_parser = StrOutputParser()
 # Cliente de Telegram
 
 client = TelegramClient(sessions.MemorySession(), API_ID_TELEGRAM, API_HASH_TELEGRAM)  
 
 
 # Clases
-
-explainer = Explainer()
+management_detoxigramers = ManagementDetoxigramers
+explainer = Explainer(mistral, output_parser, )
 analyzer = Analyzer()
 detoxifier = Detoxifier()
 dataviz = ToxicityDataviz()
