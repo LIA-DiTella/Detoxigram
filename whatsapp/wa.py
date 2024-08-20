04f838fcfb3e13df962e8a3a977ac4121fd7cbe1
PattoPeraltta <136608427+PattoPeraltta@users.noreply.github.com>
2024-08-20 18:00:48 -0300

    Debugging lack of reply of the bot

--- whatsapp/wa.py
+++ whatsapp/wa.py
@@ -6,8 +6,8 @@ from fastapi import FastAPI, Request, HTTPException
 from pywa import WhatsApp, filters
 from pywa.types import Message, CallbackButton, Button, Document
 from dotenv import main, load_dotenv
-load_dotenv()
 import sys
+load_dotenv()
 import os 
 sys.path.append('..')
 from toxicity.Analyzer import WhatsApp_Analyzer
@@ -41,15 +41,15 @@ MISTRAL_API_KEY = os.environ.get('MISTRAL_API_KEY')
 management_detoxigramers = ManagementDetoxigramers_Whatsapp()
 hatebert = hate_bert_classifier('tomh/toxigen_hatebert', verbosity=True)
 multibert = multi_bert_classifier(
-    '../model_evaluation_scripts/classifiers_classes_api/multibert',
+    '/Users/patoperaltaramos/Desktop/Labo.Neuro/Detoxigram/model_evaluation_scripts/classifiers_classes_api/multibert',
     verbosity=True,
-    toxicity_distribution_path='../model_evaluation_scripts/classifiers_classes_api/toxicity_distribution_cache/multibert_distribution.json',
+    toxicity_distribution_path='/Users/patoperaltaramos/Desktop/Labo.Neuro/Detoxigram/model_evaluation_scripts/classifiers_classes_api/toxicity_distribution_cache/multibert_distribution.json',
     calculate_toxicity_distribution=False
 )
 mistral = mistral_classifier(
     mistral_api_key=MISTRAL_API_KEY,
     templatetype='prompt_template_few_shot',
-    toxicity_distribution_path='../model_evaluation_scripts/classifiers_classes_api/toxicity_distribution_cache/mistral_distribution.json',
+    toxicity_distribution_path='/Users/patoperaltaramos/Desktop/Labo.Neuro/Detoxigram/model_evaluation_scripts/classifiers_classes_api/toxicity_distribution_cache/mistral_distribution.json',
     calculate_toxicity_distribution=False,
     verbosity=True
 )
@@ -85,6 +85,7 @@ messager = WhatsApp_Messager(wa)
 
 @wa.on_message()
 def greeting(client: WhatsApp, msg: Message):
+    print(f"Received message: {msg.text}")
     user_id = msg.from_user.wa_id
     detoxigramer = WhatsApp_Detoxigramer()
     management_detoxigramers.set_detoxigramer(user_id, detoxigramer)
@@ -139,6 +140,7 @@ def click_me(client: WhatsApp, clb: CallbackButton):
 
 @wa.on_message(filters.regex(".*")) 
 def handle_user_response(client: WhatsApp, msg: Message):
+    print(f"Received message: {msg.text}")
     user_id = msg.from_user.wa_id
     user = management_detoxigramers.get(user_id)
 
